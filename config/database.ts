51061829bb1a2a188261f3c9a91f77aa18d43fc6
Bhaskar-Prasad-informa <Bhaskar.Prasad@informa.com>
2025-03-10 18:27:07 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -62,11 +62,11 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "strapi-database.clkiosm88fnq.ap-south-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Bhaskar123"),
     },
     useNullAsDefault: true,
   },
