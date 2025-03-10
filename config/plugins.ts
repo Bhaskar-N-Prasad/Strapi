f92dc7eabee828d45d1cae08da23121184fd87c2
Prasad <bhaskar.prasad@informa.com>
2025-03-10 15:37:46 +0530

    Installed pg, aws-S3 upload provider and updated the config files

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,44 @@
-export default () => ({
-     // ...
-     'color-picker': {
-        enabled: true,
-        resolve: './src/plugins/color-picker'
-      },
-      'checkbox': {
-        enabled: true,
-        resolve: './src/plugins/checkbox'
-      },
-      'html-previewer': {
-        enabled: true,
-        resolve: './src/plugins/html-previewer'
-      },
-      // ...
-});
+import { resolve } from "path";
 
+export default ({ env }) => ({
+  // Plugins Configuration
+  'color-picker': {
+    enabled: true,
+    resolve: './src/plugins/color-picker'
+  },
+  'checkbox': {
+    enabled: true,
+    resolve: './src/plugins/checkbox'
+  },
+  'html-previewer': {
+    enabled: true,
+    resolve: './src/plugins/html-previewer'
+  },
+  'strapi-populate': {
+    enabled: true,
+    resolve: './src/plugins/strapi-populate'
+  },
+
+  // Upload Configuration
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+      },
+      actionOptions: {
+        upload: {
+          ACL: null
+        },
+        uploadStream: {
+          ACL: null
+        },
+      }
+    },
+  },
+});
